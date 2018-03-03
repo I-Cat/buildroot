174ca29e3cfdfaaf27a90d265498424a4ce4b5d4
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-03-03 11:22:51 +0100

    package/kodi-peripheral-*: mass version bump

--- package/kodi-peripheral-xarcade/kodi-peripheral-xarcade.mk
+++ package/kodi-peripheral-xarcade/kodi-peripheral-xarcade.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PERIPHERAL_XARCADE_VERSION = f1875ba4b7aa6ce85ec40fedf48ed5501c79e5fd
+KODI_PERIPHERAL_XARCADE_VERSION = d218f0dc1453477baf4ce2cc560a40736070f26c
 KODI_PERIPHERAL_XARCADE_SITE = $(call github,kodi-game,peripheral.xarcade,$(KODI_PERIPHERAL_XARCADE_VERSION))
 KODI_PERIPHERAL_XARCADE_LICENSE = GPL-2.0+
 KODI_PERIPHERAL_XARCADE_LICENSE_FILES = src/addon.cpp
