5a99fe50dabab0a0a6d6b1554b0de1648368b1f5
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-06-29 17:06:26 +0200

    package/giflib: security bump version to 5.2.1
    Version 5.1.5 fixes CVE-2018-11490
    https://sourceforge.net/p/giflib/code/ci/900d783def011e8d9f261db6839113425bf3334f/
    
    Added license hash.
    
    Upstream only provides a .gz tarball, so remove the .bz2 option.
    
    Switched package to generic-package after autoconf removal:
    https://sourceforge.net/p/giflib/code/ci/5fdd280d0049b7ee70f2ef1a8100b1473086e3eb/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>

--- package/giflib/giflib.mk
+++ package/giflib/giflib.mk
@@ -4,8 +4,7 @@
 #
 ################################################################################
 
-GIFLIB_VERSION = 5.1.4
-GIFLIB_SOURCE = giflib-$(GIFLIB_VERSION).tar.bz2
+GIFLIB_VERSION = 5.2.1
 GIFLIB_SITE = http://downloads.sourceforge.net/project/giflib
 GIFLIB_INSTALL_STAGING = YES
 GIFLIB_LICENSE = MIT
@@ -18,7 +17,21 @@ GIFLIB_BINS = \
 	gifrsize gifspnge giftext giftool gifwedge icon2gif raw2gif rgb2gif \
 	text2gif
 
-GIFLIB_CONF_ENV = ac_cv_prog_have_xmlto=no
+define GIFLIB_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define HOST_GIFLIB_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define GIFLIB_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) PREFIX=/usr install
+endef
+
+define GIFLIB_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) PREFIX=/usr install
+endef
 
 define GIFLIB_BINS_CLEANUP
 	rm -f $(addprefix $(TARGET_DIR)/usr/bin/,$(GIFLIB_BINS))
@@ -26,5 +39,9 @@ endef
 
 GIFLIB_POST_INSTALL_TARGET_HOOKS += GIFLIB_BINS_CLEANUP
 
-$(eval $(autotools-package))
-$(eval $(host-autotools-package))
+define HOST_GIFLIB_INSTALL_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(HOST_DIR) PREFIX=/ install
+endef
+
+$(eval $(generic-package))
+$(eval $(host-generic-package))
