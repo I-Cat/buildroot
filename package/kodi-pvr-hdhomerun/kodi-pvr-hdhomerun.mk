c91eb0c29bc29e7a17a9ab088ff1c4b76d57d15f
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-01-31 19:04:41 +0100

    package/kodi-pvr-*: mass version bump
    Switched license file to LICENSE.md.
    
    kodi-pvr-iptvsimple: Added dependency to pugixml:
    https://github.com/kodi-pvr/pvr.iptvsimple/commit/b4e292f11c7c3f40b8c7ff18f28f7b2e149c6bee

--- package/kodi-pvr-hdhomerun/kodi-pvr-hdhomerun.mk
+++ package/kodi-pvr-hdhomerun/kodi-pvr-hdhomerun.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-KODI_PVR_HDHOMERUN_VERSION = 3.5.0-Leia
+KODI_PVR_HDHOMERUN_VERSION = 4.1.5-Matrix
 KODI_PVR_HDHOMERUN_SITE = $(call github,kodi-pvr,pvr.hdhomerun,$(KODI_PVR_HDHOMERUN_VERSION))
 KODI_PVR_HDHOMERUN_LICENSE = GPL-2.0+
-KODI_PVR_HDHOMERUN_LICENSE_FILES = debian/copyright
+KODI_PVR_HDHOMERUN_LICENSE_FILES = LICENSE.md
 KODI_PVR_HDHOMERUN_DEPENDENCIES = jsoncpp kodi-platform libhdhomerun
 
 $(eval $(cmake-package))
