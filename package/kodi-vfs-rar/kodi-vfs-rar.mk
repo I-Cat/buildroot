3f58d88f8baa095ce0e1c4b1adc6f1bb7d66f19d
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-01-31 17:02:10 +0100

    package/kodi-vfs-*: mass version bump

--- package/kodi-vfs-rar/kodi-vfs-rar.mk
+++ package/kodi-vfs-rar/kodi-vfs-rar.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-KODI_VFS_RAR_VERSION = 60f92ff28ee6c94211b628990696c60518bffcf6
+KODI_VFS_RAR_VERSION = 3.1.4-Matrix
 KODI_VFS_RAR_SITE = $(call github,xbmc,vfs.rar,$(KODI_VFS_RAR_VERSION))
 KODI_VFS_RAR_LICENSE = unrar, GPL-2.0+
-KODI_VFS_RAR_LICENSE_FILES = lib/UnrarXLib/license.txt debian/copyright
+KODI_VFS_RAR_LICENSE_FILES = debian/copyright lib/UnrarXLib/license.txt
 KODI_VFS_RAR_DEPENDENCIES = libplatform kodi
 
 $(eval $(cmake-package))
