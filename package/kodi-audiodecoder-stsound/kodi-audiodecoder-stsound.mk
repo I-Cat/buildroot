5125d4faf998ebf75f119d58b545636b4214113d
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-02-16 12:32:31 +0100

    package/kodi-audiodecoder-*: mass version bump

--- package/kodi-audiodecoder-stsound/kodi-audiodecoder-stsound.mk
+++ package/kodi-audiodecoder-stsound/kodi-audiodecoder-stsound.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_AUDIODECODER_STSOUND_VERSION = 2.0.2-Leia
+KODI_AUDIODECODER_STSOUND_VERSION = 3.0.0-Matrix
 KODI_AUDIODECODER_STSOUND_SITE = $(call github,xbmc,audiodecoder.stsound,$(KODI_AUDIODECODER_STSOUND_VERSION))
 KODI_AUDIODECODER_STSOUND_LICENSE = GPL-2.0+
 KODI_AUDIODECODER_STSOUND_LICENSE_FILES = debian/copyright
