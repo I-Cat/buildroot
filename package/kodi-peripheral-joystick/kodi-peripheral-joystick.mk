93e2dab3c75f925044dd1fbd6b0cbe4ef36954c0
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-02-16 12:35:57 +0100

    package/kodi-peripheral-joystick: bump version to 1.5.2-Matrix

--- package/kodi-peripheral-joystick/kodi-peripheral-joystick.mk
+++ package/kodi-peripheral-joystick/kodi-peripheral-joystick.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PERIPHERAL_JOYSTICK_VERSION = 1.4.9-Leia
+KODI_PERIPHERAL_JOYSTICK_VERSION = 1.5.2-Matrix
 KODI_PERIPHERAL_JOYSTICK_SITE = $(call github,xbmc,peripheral.joystick,$(KODI_PERIPHERAL_JOYSTICK_VERSION))
 KODI_PERIPHERAL_JOYSTICK_LICENSE = GPL-2.0+
 KODI_PERIPHERAL_JOYSTICK_LICENSE_FILES = debian/copyright
