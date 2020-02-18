ab19837a692d262341db9673f1473058ec1db195
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-02-18 22:17:42 +0100

    package/kodi-screensaver-*: mass version bump
    Switch _LICENSE_FILES to debian/copyright instead of using a source
    code file.

--- package/kodi-screensaver-stars/kodi-screensaver-stars.mk
+++ package/kodi-screensaver-stars/kodi-screensaver-stars.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_STARS_VERSION = 2.1.3-Leia
+KODI_SCREENSAVER_STARS_VERSION = 2.2.1-Matrix
 KODI_SCREENSAVER_STARS_SITE = $(call github,xbmc,screensaver.stars,$(KODI_SCREENSAVER_STARS_VERSION))
 KODI_SCREENSAVER_STARS_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_STARS_LICENSE_FILES = debian/copyright
